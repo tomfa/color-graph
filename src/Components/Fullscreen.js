91053a3201c2ac66cd87026fb1c5408b05a6312c
Tomas Fagerbekk <tomas@otovo.com>
2019-12-30 20:14:52 +0100

    Add FullScreen component

---
+++ src/Components/Fullscreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { size } from '../utils/styles';
+
+const FullscreenWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+
+  @media ${size.large} {
+    overflow: hidden;
+  }
+  background-color: #101010;
+`;
+
+const InnerFullScreen = styled.div`
+  vertical-align: middle;
+  width: 100%;
+`;
+
+export const FullScreen = ({ children }) => (
+  <FullscreenWrapper>
+    <InnerFullScreen>{children}</InnerFullScreen>
+  </FullscreenWrapper>
+);
