04d8522429af406243aaa6c298a5decd54f7c9e2
Tomas Fagerbekk <tomas@otovo.com>
2020-01-02 15:24:47 +0100

    Add dummy fixed Header component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import { FullScreen } from "./Components/Fullscreen";
 import { Lines } from "./Components/Lines";
 import { randomData } from "./data";
+import { FixedSection, Header, SubHeader } from "./Components/FixedSection";
 
 export const App = () => (
   <FullScreen>
+    <FixedSection>
+      <SubHeader>d3-scale-chromatic</SubHeader>
+      <Header>ColorPalette</Header>
+    </FixedSection>
     <Lines data={randomData} />
   </FullScreen>
 );
---
+++ src/Components/FixedSection.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  font-size: 2rem;
+  margin: 1rem 0;
+`;
+
+export const SubHeader = styled.h2`
+  display: block;
+  font-size: 1rem;
+  opacity: 0.7;
+`;
+
+export const FixedSection = styled.section`
+  position: fixed;
+  padding: 40px;
+`;
